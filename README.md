e20b36e827245f7de172211b63e08e47b3cce8c8
Javier Ocampo <ferroxido@gmail.com>
2019-02-23 17:09:34 -0300

    Setup project :rocket:

---
+++ README.md
@@ -0,0 +1,3 @@
+### Simple React project setup without use CRA
+
+This repository was created in order to learn how setup a simple React project without use CRA.
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const HtmlWepackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+      },
+    ],
+  },
+  devServer: {
+    port: 8001,
+  },
+  plugins: [
+    new HtmlWepackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+};
